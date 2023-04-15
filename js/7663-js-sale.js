8bb8bd3d04042c5ecc49f9674aeaac16d2ad91af
Keanu Evanz <94647238+NEOTECNO@users.noreply.github.com>
2023-04-15 03:21:26 -0300

    Update 7663-js-sale.js

--- js/7663-js-sale.js
+++ js/7663-js-sale.js
@@ -207,10 +207,10 @@ const connect = async (e)=> {
   if (typeof window.ethereum !== 'undefined') {
     console.log('MetaMask is installed!');
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-	account = accounts[0];
+    account = accounts[0];
     
     if (account.length > 0) {
-    	document.getElementById("connect_button").innerHTML = account[0].substr(0,10) + "..." + account[0].substr(-4) ;
+    	document.getElementById("connect_button").innerHTML = accounts[0].substr(0,10) + "..." + accounts[0].substr(-4) ;
 
   		const web3 = new Web3(window.ethereum);
   		const contract = new web3.eth.Contract(abi, CONTRACT_ADDR, {gas: 3000000});
